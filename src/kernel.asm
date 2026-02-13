5d57fb219f85904bf54d0a79c19192797d022956
eshon <eshaan2031@icloud.com>
2026-02-13 11:38:24 -0600

    more stuff

--- src/kernel.asm
+++ src/kernel.asm
@@ -80,13 +80,16 @@ ap_pm_entry:
     sub esp, eax
     jmp parallel
 
+make_triangle:
 
 make_cube:
     %define make_cube_ARG_struc_addr eax
-    %define make_cube_ARG_v_arr_addr ebx
-    ; uses xmm0, xmm1, xmm2
+    %define make_cube_ARG_v_arr_addr edx
+    %define make_cube_ARG_triangle_parameters esi
+    ; also uses xmm0, xmm1, xmm2, ecx, edi
 
     vmovaps xmm0, [make_cube_ARG_struc_addr]
+    ; xmm1: [s, s, s, s]
     vbroadcastss xmm1, dword [make_cube_ARG_struc_addr + CUBE.size]
     vsubps xmm2, xmm0, xmm1
     vmovaps [make_cube_ARG_v_arr_addr + 16*0], xmm2
@@ -106,6 +109,11 @@ make_cube:
     vaddps xmm2, xmm0, xmm1
     vmovaps [make_cube_ARG_v_arr_addr + 16*7], xmm2
 
+    ; xmm1:  0, 1, 2, 3
+    ; xmm1: [5, 6, 7, 8]
+    ; xmm1: [5, 7, 6, 6]
+    ;        0, 2, 1, 1
+    ;        0b00100101
     vshufps xmm1, xmm1, xmm1, 0b00001011
     ; xmm1: [-s, -s, s, s]
 
@@ -123,6 +131,27 @@ make_cube:
 
     vaddps xmm2, xmm0, xmm1
     vmovaps [make_cube_ARG_v_arr_addr + 16*5], xmm2
+
+    xor ecx, ecx
+make_cube_for_values_in_idx:
+    ; ecx + GLOBAL_MAKE_CUBE_INDEX + 4*0/1/2
+    mov edi, [ecx + GLOBAL_MAKE_CUBE_INDEX + 4*0] ; i1
+    mov edi, [edi*4 + make_cube_ARG_v_arr_addr]
+    mov [make_cube_ARG_triangle_parameters], edi
+
+    mov edi, [ecx + GLOBAL_MAKE_CUBE_INDEX + 4*1] ; i2
+    mov edi, [edi*4 + make_cube_ARG_v_arr_addr]
+    mov [make_cube_ARG_triangle_parameters + 4], edi
+
+    mov edi, [ecx + GLOBAL_MAKE_CUBE_INDEX + 4*2] ; i3
+    mov edi, [edi*4 + make_cube_ARG_v_arr_addr]
+    mov [make_cube_ARG_triangle_parameters + 4*2], edi
+
+    call make_triangle
+
+    add ecx, 12
+    cmp ecx, 144
+    jl make_cube_for_values_in_idx
 
     ret
 
@@ -159,14 +188,36 @@ parallel:
     %define COL_SKY_2 2.55
 
     cmp CORE_NUM, 0
-    jne root_setup_end
+    jmp root_setup_end
+
 
 root_setup: ; before the first frame begins
     mov make_cube_ARG_struc_addr, cube_1
-    mov make_cube_ARG_v_arr_adr, make_cube_v
+    mov make_cube_ARG_v_arr_addr, make_cube_v_1
+    mov make_cube_ARG_triangle_parameters, make_cube_1_triangle_parameters
+    call make_cube
+
+    mov make_cube_ARG_struc_addr, cube_2
+    mov make_cube_ARG_v_arr_addr, make_cube_v_2
+    mov make_cube_ARG_triangle_parameters, make_cube_2_triangle_parameters
     call make_cube
     
 root_setup_end:
+    mov edi, [lfb_addr]
+
+    mov byte [edi], 0x00
+    mov byte [edi+1], 0x00
+    mov byte [edi+2], 0xFF
+    mov edx, ebx
+    imul edx, 3
+    add edi, edx
+    mov edx, 1600*3
+    imul edx, ebx
+    add edi, edx
+
+    mov byte [edi], 0x00
+    mov byte [edi+1], 0x00
+    mov byte [edi+2], 0xFF
 
     cli
     hlt
@@ -178,7 +229,7 @@ align 4
 lfb_addr:  dd 0
 mode_info: times 256 db 0
 num_total_cores: dd 0
-XMM_GV_SIGN_MASK: dd 0x80000000
+XMM_GV_SIGN_MASK: dd 0x80000000 ; 10000000000000000000000000000000000000000000000
 GLOBAL_MAKE_CUBE_INDEX: 
     dd 0, 1, 2
     dd 0, 2, 3
@@ -194,49 +245,49 @@ GLOBAL_MAKE_CUBE_INDEX:
     dd 3, 6, 7
 
 struc CUBE
-  .pos_x resb 4
-  .pos_y resb 4
-  .pos_z resb 4
+  .pos_x: resb 4
+  .pos_y: resb 4
+  .pos_z: resb 4
   
-  .size resb 4
+  .size: resb 4
   
-  .color_x resb 4
-  .color_y resb 4
-  .color_z resb 4
+  .color_x: resb 4
+  .color_y: resb 4
+  .color_z: resb 4
 
-  .mat resb 4
+  .mat: resb 4
 endstruc
 
-align 32
+align 32 ; ymm registers
 cube_1:
   istruc CUBE
-    at .pos_x dd -1.4
-    at .pos_y dd 0.8
-    at .pos_z dd 0.0
+    at .pos_x, dd -1.4
+    at .pos_y, dd 0.8
+    at .pos_z, dd 0.0
     
-    at .size dd 0.8
+    at .size, dd 0.8
 
-    at .color_x dd COL_MIRROR_0
-    at .color_y dd COL_MIRROR_1
-    at .color_z dd COL_MIRROR_2
+    at .color_x, dd COL_MIRROR_0
+    at .color_y, dd COL_MIRROR_1
+    at .color_z, dd COL_MIRROR_2
 
-    at .mat dd 1
+    at .mat, dd 1
   iend
 
 align 32
 cube_2:
   istruc CUBE
-    at .pos_x dd 1.4
-    at .pos_y dd 0.8
-    at .pos_z dd 0.0
+    at .pos_x, dd 1.4
+    at .pos_y, dd 0.8
+    at .pos_z, dd 0.0
     
-    at .size dd 0.8
+    at .size, dd 0.8
 
-    at .color_x dd COL_LIGHT_0
-    at .color_y dd COL_LIGHT_1
-    at .color_z dd COL_LIGHT_2
+    at .color_x, dd COL_LIGHT_0
+    at .color_y, dd COL_LIGHT_1
+    at .color_z, dd COL_LIGHT_2
 
-    at .mat dd 2
+    at .mat, dd 2
   iend
 
 align 8
@@ -255,3 +306,6 @@ section .bss
 align 32
 make_cube_v_1: resb 4*4*8
 make_cube_v_2: resb 4*4*8
+
+make_cube_1_triangle_parameters: resb 4*3*2 + 4 ; 28 in total, not 32 aligned anymore
+make_cube_2_triangle_parameters: resb 4*3*2 + 4 ; 28 in total, not 32 aligned anymore
